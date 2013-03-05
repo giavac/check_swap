b637797038d19f40a13aae7197c6c505f6c7db0e
Giacomo Vacca <giacomo.vacca@gmail.com>
2013-03-05 17:11:44 +0000

    Refactoring

--- check_swap.py
+++ check_swap.py
@@ -1,29 +1,36 @@
+# check_swap
+# Giacomo Vacca
+
 import os
 import re
 import subprocess
 
+def check_swap(pattern):
+    total = 0
+
+    # for each directory in /proc/...
+    for dirname in os.listdir("/proc/"):
+        # if it is numeric...
+        if (dirname.isdigit()):
+            # get the command name for that process
+            process_name = subprocess.Popen(["ps", "-p", dirname, "--no-headers", "-o", "comm"], stdout=subprocess.PIPE).communicate()[0]
+
+            if (re.match(pattern, str(process_name))):
+
+                swap = subprocess.Popen(["grep", "Swap", "/proc/" + dirname + "/smaps"], stdout=subprocess.PIPE).communicate()[0]
+                swap_list = swap.split()
+                swap_list[:] = (value for value in swap_list if (value != "Swap:" and value != "kB"))
+                partial = sum(int(value) for value in swap_list)
+                print process_name.rstrip() + ": " + str(partial) + " kB"
+                total += partial
+            else:
+                # print "Skipped: ", output
+                continue
+
+    print "Total: " + str(total) + " kB"
+
+
+# ====== MAIN =============
 pattern = raw_input('Enter regex for process name (optional):')
-
-total = 0
-
-# for each directory in /proc/...
-for filename in os.listdir("/proc/"):
-    # if it is numeric...
-    if re.match("\d", filename):
-        # get the command name for that process
-        process_name = subprocess.Popen(["ps", "-p", filename, "--no-headers", "-o", "comm"], stdout=subprocess.PIPE).communicate()[0]
-
-        #pattern = "^.*$"
-        if (re.match(pattern, str(process_name))):
-            # sudo grep Swap /proc/29164/smaps
-            swap = subprocess.Popen(["grep", "Swap", "/proc/" + filename + "/smaps"], stdout=subprocess.PIPE).communicate()[0]
-            swap_list = swap.split()
-            swap_list[:] = (value for value in swap_list if (value != "Swap:" and value != "kB"))
-            partial = sum(int(value) for value in swap_list)
-            print process_name.rstrip() + ": " + str(partial) + " kB"
-            total += partial
-        else:
-            # print "Skipped: ", output
-            continue
-
-print "Total: " + str(total) + " kB"
+check_swap(pattern)
+# ====== END ==============
